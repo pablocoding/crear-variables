6b3a5283fc8628a5004ea7135e98df48a0f9e1ac
Juan Pablo Ibarbo <juanibarbo456@gmail.com>
2022-04-02 14:49:58 -0500

    version1

---
+++ Ibarbo_Juan_JS_CrearVariables.js
@@ -0,0 +1,18 @@
+var altura;
+
+var edad;
+
+// creamos las variables 
+// variables para la altura y edad de las personas
+
+ altura = 1;
+
+ edad = 10;
+
+ // les agregamos un valor a las variables 
+// valores para determinar si una persona puede subirse a la montaña rusa
+
+console.log(altura+" "+"metros");
+console.log(edad+" "+"años");
+
+
